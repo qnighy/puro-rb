5729399c14cd45587bd52cfb6cbf4352211da5ca
Masaki Hara <ackie.h.gmai@gmail.com>
2023-04-29 16:56:24 +0900

    Test https

--- spec/http_spec.rb
+++ spec/http_spec.rb
@@ -17,6 +17,17 @@ RSpec.describe "http" do # rubocop:disable RSpec/DescribeClass
       expect(content).to include("https://www.iana.org/domains/example")
     end
 
+    it "requests an HTTPS resource successfully (real)" do
+      status, headers, content = Puro::Http.request(
+        :GET,
+        "https://example.com",
+        middlewares: [Puro::BaseMiddleware]
+      )
+      expect(status).to be(200)
+      expect(headers["content-type"]).to match(%r{^text/html\b})
+      expect(content).to include("https://www.iana.org/domains/example")
+    end
+
     it "requests an HTTP resource successfully (mocked)" do
       sock = StreamMock.new(
         [
@@ -48,6 +59,39 @@ RSpec.describe "http" do # rubocop:disable RSpec/DescribeClass
       expect(content).to eq("Hello, world!")
 
       expect(middleware).to have_received(:connect_tcp).with(anything, anything, "example.com", 80).once
+    end
+
+    it "requests an HTTPS resource successfully (mocked)" do
+      sock = StreamMock.new(
+        [
+          [:read, "GET / HTTP/1.1\r\n"],
+          [:read, "host: example.com\r\n"],
+          [:read, "user-agent: test\r\n"],
+          [:read, "accept: text/html\r\n"],
+          [:read, "\r\n"],
+          [:write, "HTTP/1.1 200 OK\r\n"],
+          [:write, "Content-Type: text/html; charset=UTF-8\r\n"],
+          [:write, "Content-Length: 13\r\n"],
+          [:write, "\r\n"],
+          [:write, "Hello, world!"],
+          [:close]
+        ]
+      )
+
+      middleware = Object.new
+      middleware.extend(Puro::Middleware)
+      allow(middleware).to receive(:connect_tls).and_return(sock)
+
+      status, headers, content = Puro::Http.request(
+        :GET,
+        "https://example.com",
+        middlewares: [middleware, Puro::BaseMiddleware]
+      )
+      expect(status).to be(200)
+      expect(headers["content-type"]).to match(%r{^text/html\b})
+      expect(content).to eq("Hello, world!")
+
+      expect(middleware).to have_received(:connect_tls).with(anything, anything, "example.com", 443).once
     end
   end
 end
